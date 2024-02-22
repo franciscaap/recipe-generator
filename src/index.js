6b2801a103a19f1b76a2c20d39d8f054294fd45a
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-02-22 20:31:49 +0000

    Added the AI API integration.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings:
-      "Directions: 1 Bring the balsamic vinegar to a boil over medium-low heat in a small saucepan.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let userInstructions = document.querySelector("#user-instructions");
+  let apiKey = "9b00a0b2o792ct546c043d35bf49a6e3";
+  let prompt = `uesr instructions: Please generate an italian cuisine recipe using ${userInstructions.value}. Generate it in basic HTML format and seperate instuctions from ingredients. Wrap the recipe name in html strong. Please do not acknowlege user.`;
+  let context =
+    "you are an Italian culinary exper and you give short recipes. You make sure to follow the user instructions when generating the recipes.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
