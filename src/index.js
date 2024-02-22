bae33ff32a3b5caba28fcf5f9d5a975203587e74
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-02-22 18:35:28 +0000

    Added form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings:
+      "Directions: 1 Bring the balsamic vinegar to a boil over medium-low heat in a small saucepan.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
